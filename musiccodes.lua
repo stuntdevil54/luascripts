55f6ac6cf786f97102910b063ea06848734f77f3
stuntdevil54 <luckyshot892@gmail.com>
2025-04-19 12:34:09 -0700

    Create musiccodes.lua

---
+++ musiccodes.lua
@@ -0,0 +1,34 @@
+--// 2025 Bypassed and Awesome audios \\--
+
+--// Lobby Music \\--
+--[[
+
+rbxassetid://1842241530
+rbxassetid://1845756489
+rbxassetid://1845458027
+rbxassetid://1837070127
+rbxassetid://1846458016
+rbxassetid://1846575559
+rbxassetid://1838857104
+--]]
+--//=========\\--]]
+
+--// Bypassed Music \\--
+--[[
+rbxassetid://16831105312 -- This Slaps Like DAMN
+rbxassetid://7388737421 -- FETCH ME THEIR SOULS!
+rbxassetid://86265966611265 -- I'm not racist lol
+rbxassetid://83421404752400 -- Plankton
+rbxassetid://6470733039 -- Vid 1444
+rbxassetid://105459428035296 -- Ronnie MCNutinmymouth
+rbxassetid://97737727215977 -- Female Moans
+rbxassetid://103232206651699 -- Gay
+rbxassetid://3824308862 -- N Word
+rbxassetid://103456533543835 -- Comet
+rbxassetid://76463442516219 -- China
+rbxassetid://1837492065 -- Nevah take mah skazoo
+rbxassetid://79099052007893 -- Doge!
+--]]
+--//================\\--
+
+--// Thanks for using this list! Made by Blockhead (。・ω・。) \\--
