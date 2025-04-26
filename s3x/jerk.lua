e41daddcbfaf721efe6adbbc919e93c3d217f725
stuntdevil54 <luckyshot892@gmail.com>
2025-04-26 10:42:36 -0700

    Add files via upload

---
+++ s3x/jerk.lua
@@ -0,0 +1,11 @@
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+local character = player.Character
+	
+if character:FindFirstChild("UpperTorso") then
+print("R15")
+loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
+elseif character:FindFirstChild("Torso") then
+print("R6")
+loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))()
+end
